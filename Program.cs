3657d2af4af8a942e3165e8910615d1de696fd8d
Muhammad <99860630+muhammadgith@users.noreply.github.com>
2022-04-14 09:03:21 +0500

    Csharp

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿int number, secondNum, counter=0;
+number = Convert.ToInt32(Console.ReadLine());
+int[] Arr = new int[number];
+for(int i=0; i<number; i++)
+{
+    Arr[i] = Convert.ToInt32(Console.ReadLine());
+
+}
+
+secondNum = Convert.ToInt32(Console.ReadLine());
+
+for(int i=0; i<number; i++)
+{
+    if(Arr[i]==secondNum)
+    {
+        counter++;
+    }
+}
+
+Console.WriteLine($"{counter}");
+
+
+
+Console.ReadKey();
